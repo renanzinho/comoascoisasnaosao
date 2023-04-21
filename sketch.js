eb530128d466bf6e8c1b1dc0507236fa447c81e3
renan <rdefreitaslins@apple.com>
2023-04-20 21:49:55 -0300

    WIP

--- sketch.js
+++ sketch.js
@@ -1,29 +1,38 @@
 /////////// Forniture
 class Forniture {
-  constructor(x, y, w, h, rotation, name) {
-    this.x = x;
-    this.y = y;
-    this.width = w;
-    this.height = h;
-    this.rotation = rotation;
-    this.over = false;
-    this.name = name;
+  constructor(x, y, w, h, rotation, img, name) {
+    this.img = img
+    this.x = x
+    this.y = y
+    this.width = w
+    this.height = h
+    this.rotation = rotation
+    this.over = false
+    this.name = name
+    this.p
   }
-  
+
   display() {
     push()
     
     translate(this.x, this.y);
     rotate(this.rotation);
     this.containsPoint();
-    if(this.over) {
-      fill(55);
-    } else {
-      fill(255);
-    }
-    rect(0, 0, this.width, this.height);
-    
+
+    image(this.img, 0, 0, this.width, this.height, 0, 0, this.img.width, this.img.height, COVER, CENTER)
+
     pop()
+
+    if (this.over) {
+      push()
+
+      textSize(18)
+      fill('#8A9085')
+      rectMode(CENTER)
+      text(this.name, this.x + this.width/2, this.y - 5, 60, 30)
+
+      pop()
+    }
   }
   
   sign(p1, p2, p3) {
@@ -76,14 +85,13 @@ class Forniture {
     
     this.over = this.pointInTriangle(mousePoint, triangle1) || this.pointInTriangle(mousePoint, triangle2)
   }
+
+  pressed() {
+    if(this.over) {
+      state = this.name
+    }
+  }
 }
-
-
-/////////// set objects
-let fan = new Forniture(556, 328, 52, 46, 79, "fan")
-let tv = new Forniture(462, 108, 85, 61, 39, "tv")
-let wind = new Forniture(292, 18, 84, 49, 0, "wind")
-let sofa = new Forniture(230, 210, 222, 91, 37, "sofa")
 
 /////////// Start state
 class StartButton {
@@ -91,47 +99,83 @@ class StartButton {
     push()
     let btn = createButton('Start')
     btn.mousePressed(function() {
-      console.log(state)
       state = States.main
       getAudioContext().resume()
       streetSound.loop()
     })
-    btn.position(300, 475/2-50)
+    btn.position(300, 457/2-50)
     pop()
   }
 }
 
 let startButtonRef = new StartButton()
 
+/////////// Sofa state
+class Sofa {
+  display() {
+    push()
+
+    let wrapper = select('#player-size')
+    let player = select('#player-wrapper')
+    let iframe = createElement('iframe')
+    iframe.id('player')
+    iframe.attribute('src', 'https://www.youtube.com/embed/eJP9MRr2YWM?&vq=hd1080ptheme=dark&autoplay=1&autohide=2&modestbranding=1&fs=0&showinfo=0&rel=0&iv_load_policy=3')
+    iframe.attribute('width', '400')
+    iframe.attribute('height', '235')
+    iframe.attribute('frameborder', '0')
+    iframe.attribute('allow', 'autoplay')
+    player.child(iframe)
+    wrapper.position(canvasWidth/2-760/2, canvasHeight/2 - 428/2 - 5)
+    wrapper.removeAttribute('hidden')
+
+    pop()
+  }
+}
+
 /////////// Preload assets
-let streetSound
+let streetSound, sofaImg, windowImg, fanImg, tvImg, frameImg
+let canvasWidth = 800
+let canvasHeight = 457
 
 function preload() {
   streetSound = loadSound('assets/audioRuaInicial.mp3')
   streetSound.setVolume(0.1)
+  sofaImg = loadImage('assets/sofa.png')
+  windowImg = loadImage('assets/janela.png')
+  fanImg = loadImage('assets/ventilador.png')
+  tvImg = loadImage('assets/tv.png')
+  frameImg = loadImage('assets/frame.png')
 }
 
 /////////// UI State
 const States = {
-  start: 0,
-  main: 1,
-  sofa: 2,
+  start: 'start',
+  main: 'main',
+  sofa: 'sofa',
 }
 
-let state = States.start
+let state = States.main
 let uiItems = []
 
+let fan, tv, wind, sofa, sofaState
 
 /////////// Canvas
 function setup() {
+  fan = new Forniture(524, 228, 52, 118, -2, fanImg, "ventilador")
+  tv = new Forniture(442, 108, 95, 83, -3, tvImg, "tv")
+  wind = new Forniture(252, 28, 99, 93, 0, windowImg, "janela")
+  sofa = new Forniture(230, 230, 243, 116, 8, sofaImg, "sofa")
+  sofaState = new Sofa()
+
   createCanvas(800, 457)
-  background(230)
   frameRate(60)
   angleMode(DEGREES)
   outputVolume(0.8)
 }
 
 function draw() {
+  background(255)
+  image(frameImg, 0, 0, 800, 457, 0, 0, frameImg.width, frameImg.height, CONTAIN, CENTER)
   removeElements()
   uiItems = []
   
@@ -142,13 +186,21 @@ function draw() {
     case States.main:
       uiItems.push(fan, tv, wind, sofa)
       break
+    case States.sofa:
+      uiItems.push(sofaState)
+      break
   }
 
   uiItems.forEach(function(ele) {
     ele.display()
   })
+  if (state==States.sofa) {
+    noLoop()
+  }
 }
 
 function mousePressed() {
-  // noLoop();
+  uiItems.forEach(function(ele) {
+    ele.pressed()
+  })
 }
